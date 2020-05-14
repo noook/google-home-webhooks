47d1113c486059afb3d3cebcf7f5aa8a96610ec8
noook <me@neilrichter.com>
2020-05-14 23:08:28 +0200

    🎉 Can now wake up computer with voice !

---
+++ README.md
@@ -0,0 +1,47 @@
+# Google Home Webhooks
+
+## Context
+
+I just bought a Google Home Mini, and was wondering what was possible to make with it.
+The goal was mainly to trigger code behind voice, so I tried to trigger something that I already
+do through CLI: turning on my computer remotely with a WakeOnLAN.
+
+I also heard about [IFTTT](https://ifttt.com) that allows you to automate a lot of things,
+and an interesting feature is the webhooks. They allow me to make HTTP requests with JSON body
+to a given URL, when an event (Google Home action) occurs.
+
+## Idea
+
+Webhooks are not secure, and everybody with the URL can trigger it, so I had to find a way
+to be authenticated so only IFTTT can make requests. So I created a command that generates
+a [JWT](https://en.wikipedia.org/wiki/JSON_Web_Token) that also attaches data to it. So when
+the webhook receives the request, I can verify that I am the creator of the token, and can also 
+read what's inside it. In this example, I'm attaching the MAC address of my computer.
+
+## Usage
+
+Setup your environment variables either by exporting it in your shell or by creating a `.env` file.
+You need to fill in the `JWT_SECRET` variable.
+
+You can build the command then run it with :
+```sh
+go build -o ifttt-wol
+
+./ifttt-wol generate <your-mac-address-here>
+# ey.................
+```
+
+Keep this token and configure your IFTTT action, then pass the token in the `POST` request JSON
+payload:
+```json
+{
+  "token": "<your-token-here>"
+}
+```
+
+Then run the server with
+```sh
+./ifttt-wol server
+```
+
+Your server is ready to accept hooks !
---
+++ main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"os/exec"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/joho/godotenv"
+	"github.com/thatisuday/commando"
+)
+
+type Claims struct {
+	Command string `json:"command"`
+	Arg     string `json:"arg"`
+	jwt.StandardClaims
+}
+
+type TokenPayload struct {
+	Token string `json:"token"`
+}
+
+var jwtSecret []byte
+
+func Generate(args map[string]commando.ArgValue, flags map[string]commando.FlagValue) {
+	macAddress := args["mac"].Value
+
+	expirationTime := time.Now().Add(time.Minute * 60 * 24 * 30)
+	claims := &Claims{
+		Command: "WAKEUP",
+		Arg:     macAddress,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtSecret)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(tokenString)
+}
+
+func Server(args map[string]commando.ArgValue, flags map[string]commando.FlagValue) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		var token TokenPayload
+
+		err := json.NewDecoder(r.Body).Decode(&token)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		// Do something with the Person struct...
+		claims := jwt.MapClaims{}
+		data, err := jwt.ParseWithClaims(token.Token, claims, func(token *jwt.Token) (interface{}, error) {
+			return jwtSecret, nil
+		})
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		if claims, ok := data.Claims.(jwt.MapClaims); ok && data.Valid {
+			if str, ok := claims["command"].(string); ok && str == "WAKEUP" {
+				if str, ok := claims["arg"].(string); ok {
+					cmd := exec.Command("zsh", "-c", fmt.Sprintf("wakeonlan %s", str))
+					if err := cmd.Run(); err != nil {
+						fmt.Println(err)
+					}
+				}
+			}
+		} else {
+			fmt.Println(err)
+		}
+	})
+	http.ListenAndServe(":10100", nil)
+}
+
+func main() {
+	godotenv.Load()
+	jwtSecret = []byte(os.Getenv("JWT_SECRET"))
+
+	commando.
+		SetExecutableName("ifttt-wol").
+		SetDescription("Creates an http server able to wake an ethernet wired device")
+
+	commando.
+		Register("generate").
+		AddArgument("mac", "MAC address of the device it should wake up", "").
+		SetDescription("Generates a JWT that will be accepted to use the WakeOnLAN utility").
+		SetShortDescription("Generates a JWT that will be accepted to use the WakeOnLAN utility").
+		SetAction(Generate)
+
+	commando.
+		Register("server").
+		SetDescription("Runs the server that intercepts the Webhook").
+		SetShortDescription("Runs the server that intercepts the Webhook").
+		SetAction(Server)
+
+	commando.Parse(nil)
+}
